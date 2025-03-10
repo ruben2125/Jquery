f49adca910b5e5ce56aec795943590114b9cf3fd
ruben2125 <undergroundvibes25@gmail.com>
2025-03-10 09:07:14 +0100

    cambios js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    $(document).ready(function() {
+      $("#toggle-btn").click(function() {
+        $("#parrafo").toggle(500); // Alterna la visibilidad con una animación de 500 ms
+      });
+    });
+ 
